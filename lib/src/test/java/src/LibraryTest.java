276b1caf6d217d6e8786a17c1d6fa159bb705ae0
Einar Kjellback <einar.kjellback@avinor.no>
2021-11-25 12:49:07 +0100

    commit before push

--- lib/src/test/java/src/LibraryTest.java
+++ lib/src/test/java/src/LibraryTest.java
@@ -3,12 +3,14 @@
  */
 package src;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LibraryTest {
-    @Test public void someLibraryMethodReturnsTrue() {
+    @Test
+    public void someLibraryMethodReturnsTrue() {
         Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
     }
 }
